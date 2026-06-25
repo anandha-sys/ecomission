6af30cc43c61e5a7a0c110e75798b8bec9fee7ca
anandha-sys <krishnanhanandha45@gmail.com>
2026-06-25 17:46:44 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
