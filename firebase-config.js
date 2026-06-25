4e3422139e2ffd5e42f29b13442cc4e7886b053c
anandha-sys <krishnanhanandha45@gmail.com>
2026-06-25 17:51:40 +0530

    Update Firebase config with new credentials
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDTEv8RPwRgopApG7Et-tNpY_z4vW-e8ug",
+  authDomain: "ecom-3faa6.firebaseapp.com",
+  projectId: "ecom-3faa6",
+  storageBucket: "ecom-3faa6.firebasestorage.app",
+  messagingSenderId: "521605172042",
+  appId: "1:521605172042:web:7b7945217f3bdbfecd8fb3",
+  measurementId: "G-968S6E9XP1"
 };
-
-firebase.initializeApp(firebaseConfig);
